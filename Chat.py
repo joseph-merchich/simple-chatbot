295f27b6d42fef377ef4f45f03dcff895b14d969
joseph-merchich <tdury2576@gmail.com>
2026-04-14 13:30:44 -0400

    Added Chat.py
    This is the main code. Please note this code is using the llama2:7b-chat model, but this can be easily replaced with the Ollama model of your choice.

---
+++ Chat.py
@@ -0,0 +1,74 @@
+import requests
+import json
+import sys
+import time
+model = "llama2:7b-chat"
+outfile = sys.argv[2] if (len(sys.argv) == 3) else ""
+url = "http://localhost:11434/api/chat"
+sound = input("Would you like Smart Reading on? y/n")
+if sound.lower() == "y":
+    
+    try:
+        import pyttsx3
+    except ImportError:
+        print('The pyttsx3 module needs to be installed to run this')
+        print('On windows, open a commmand prompt and run: py -m pip install pyttsx3')
+        sys.exit()
+
+    tts = pyttsx3.init()
+    voice_on = True
+    
+def slowSpacePrint(text, interval=0.01):
+    for character in text:
+        print(character + '', end='',flush=True)
+        time.sleep(interval)
+    print()
+    print()
+
+def speak(text):
+    tts.say(text)
+    tts.runAndWait()
+    
+prompt = input("]")
+text = "]" + prompt + "\n"
+print()
+messages = [{
+    "role": "user",
+    "content": prompt
+    }]
+
+while (prompt != ""):
+    print("Thinking...")
+    print()
+    msg = {
+        "model": model,
+        "messages": messages,
+        "stream": False
+        }
+    resp = requests.post(url, json=msg)
+    if (resp.status_code != 200):
+        raise ValueError("Bad response from Ollama server")
+
+    reply = json.loads(resp.text)
+    ans = reply['message']['content']
+    text += ans + "\n\n"
+    slowSpacePrint(ans)
+    if voice_on == True:
+        speak(ans)
+    print()
+    messages.append({
+        "role": "assistant",
+        "content": ans
+        })
+    
+    prompt = input("]")
+    if (prompt != ""):
+        text += "]" + prompt + "\n"
+        messages.append({
+            "role": "user",
+            "content": prompt
+            })
+
+        if (outfile != ""):
+            with open(outfile, "w") as f:
+                f.write(text)
